21ef1c7c465f90febf3cd31092a564f3a6f62381
freezy7 <453901962@qq.com>
2015-06-14 13:05:24 +0800

    Initial Commit

---
+++ Ratings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by R_style_Man on 15/6/14.
+//  Copyright (c) 2015年 R_style_Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
