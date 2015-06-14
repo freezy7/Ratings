3644a881c8727b3e4fdea723de6b567a130cc617
freezy7 <453901962@qq.com>
2015-06-14 17:38:22 +0800

    Storyboards Tutorial in Swift

---
+++ Ratings/PlayerCell.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerCell.swift
+//  
+//
+//  Created by R_style_Man on 15/6/14.
+//
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+    
+    @IBOutlet weak var gameLabel:UILabel!
+    @IBOutlet weak var nameLabel:UILabel!
+    @IBOutlet weak var ratingImageView:UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Ratings/SampleData.swift
@@ -0,0 +1,15 @@
+//
+//  SampleData.swift
+//  Ratings
+//
+//  Created by R_style_Man on 15/6/14.
+//  Copyright (c) 2015年 R_style_Man. All rights reserved.
+//
+
+import Foundation
+
+//Set up sample data
+
+let playersData = [ Player(name: "Bill Evans", game: "Tic-Tac-Toe", rating: 4),
+    Player(name: "Oscar Perterson", game: "Apin the Bottle", rating: 5),
+    Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker'", rating: 2)]
